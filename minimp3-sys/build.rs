f8dada3cf7281531ac4817feb4ad4ed949240b59
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-01-02 23:36:53 +0100

    switches to using bindgen in build.rs for minimp3-sys

--- minimp3-sys/build.rs
+++ minimp3-sys/build.rs
@@ -1,4 +1,5 @@
-extern crate cc;
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
     cc::Build::new()
@@ -6,4 +7,22 @@ fn main() {
         .file("minimp3.c")
         .define("MINIMP3_IMPLEMENTATION", None)
         .compile("minimp3");
+
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("minimp3/minimp3.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
